1612b2a21544d2da3a42f84cf085dcdc1206ee43
pupil <pupil@ivkhk.ee>
2020-10-26 11:15:49 +0200

    first commit

---
+++ src/jptvr19klishinkitchen/App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr19klishinkitchen;
+
+import entity.Item;
+
+/**
+ *
+ * @author pupil
+ */
+public class App {
+    private Item[] items = new Item[];
+
+    void run() {
+        
+    }
+}
